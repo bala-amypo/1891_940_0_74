2c82c6c6c074be1b991008d10ef245f472262e56
bala-amypo <bala@amypo.in>
2025-12-18 10:03:20 +0000

    User commit

--- src/main/java/com/example/demo/Serviceimpl/StudentServiceImple.java
+++ src/main/java/com/example/demo/Serviceimpl/StudentServiceImple.java
@@ -6,17 +6,17 @@ import com.example.demo.entity.Student;
 import com.example.demo.repository.StudentRepository;
 
 @Service
-public class StudentServiceImple implements StudentService{
-
-    // private final StudentRepository studentRepository;
-    // public StudentServiceImpl(StudentRepository studentRepository){
-    //     this.studentRepository=studentRepository;
-    // }
+public class StudentServiceImpl implements StudentService {
 
     @Autowired
     StudentRepository studentRepository;
 
-    public Student saveStudent(Student student){
+    public Student saveStudent(Student student) {
         return studentRepository.save(student);
+    }
+
+    public Student getStudentById(Long id) {
+        return studentRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Student not found"));
     }
 }
